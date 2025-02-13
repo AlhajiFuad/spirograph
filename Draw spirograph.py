c0d7a59f5a69133c6890074acbc948fc25435a2e
AlhajiFuad <alhajifuad074@gmail.com>
2025-02-13 23:15:16 +0000

    modified

--- Draw spirograph.py
+++ Draw spirograph.py
@@ -1,29 +1,39 @@
 import random
 import turtle
 
+# Set up the turtle and screen
 hajj = turtle.Turtle()
 turtle.colormode(255)
 turtle.title("SPIROGRAPH")
 
 def random_color():
+    """Generate a random RGB color."""
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    color = (r, g, b)
-    return color
-
+    return (r, g, b)
 
 hajj.speed("fastest")
 
-
-def draw_spirograph(size):
+def draw_spirograph(size, radius):
+    """
+    Draw a spirograph with the given size increment and circle radius.
+    :param size: The angle increment for each circle.
+    :param radius: The radius of the circles.
+    """
     for _ in range(int(360 / size)):
         hajj.color(random_color())
-        hajj.circle(108)
-        hajj.setheading(hajj.heading() + 10)
+        hajj.circle(radius)
+        hajj.setheading(hajj.heading() + size)
 
+if __name__ == "__main__":
+    # Customize spirograph parameters
+    spirograph_size = 5  # Angle increment in degrees
+    spirograph_radius = 108  # Circle radius
 
-draw_spirograph(5)
+    # Draw the spirograph
+    draw_spirograph(spirograph_size, spirograph_radius)
 
-screen = turtle.Screen()
-screen.exitonclick()
+    # Exit on click
+    screen = turtle.Screen()
+    screen.exitonclick()
